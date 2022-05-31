835c5b0114897f2d217d0a5ea0000549d02dc6f5
João Pedro <jpedro@gmail.com>
2022-05-30 22:57:28 -0300

    Learning to share state between components of the same type

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,21 @@
-import { createSignal } from "solid-js";
-
+import { createSignal, createEffect } from "solid-js";
+// Create Signal can be used outside the component, noice, unlike React
+// This can be used to share state between components
+const [count, setCount] = createSignal(0);
 export default function Counter() {
-	// Returns a count getter and a setter
-	// I called the getter count() and the setter setCount() 
-  const [count, setCount] = createSignal(0);
+  // Returns a count getter and a setter
+  // I called the getter count() and the setter setCount()
+  const add = () => {
+    return setCount(count() + 1);
+  };
+  const remove = () => {
+    return setCount(count() - 1);
+  };
 
-	const add = () =>{
-		return setCount(count() + 1)
-	}
-	const remove = () =>{
-		return setCount(count() - 1)
-	}
+  createEffect(() => {
+    // using a reactive component in createEffect, reruns this effect
+    console.log("Teste de reatividade", count());
+  });
 
   return (
     <div>
